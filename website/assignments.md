dc35e484b57bb4194041e8b3185013d072a37996
Vivek Srikrishnan <vs498@cornell.edu>
2022-07-08 12:00:11 -0400

    add rubric badges

--- website/assignments.md
+++ website/assignments.md
@@ -11,4 +11,4 @@
 
 | Assignment | Due Date | Instructions | Rubric |
 |------------|----------|:------------:|:------:|
-| HW0 | Sep 02, 2022 | [![HTML Instructions](https://img.shields.io/static/v1?label=web&message=view&color=b31b1b&labelColor=222222&style=plastic&)](/assignments/hw0/hw0) [![PDF Instructions](https://img.shields.io/static/v1?label=pdf&message=view&color=b31b1b&labelColor=222222&style=plastic&logoColor=white)](/assignments/hw0/hw0.pdf) | Only graded for completion |
+| Homework 0 | Sep 02, 2022 | {{hw_badges 0}} | Only graded for completion |
--- website/utils.jl
+++ website/utils.jl
@@ -14,3 +14,70 @@ function lx_baz(com, _)
   # do whatever you want here
   return uppercase(brace_content)
 end
+
+
+function hw_badge(num, ftype)  
+  if ftype == "html"
+    link = string("/assignments/hw", num, "/hw", num, "/")
+    alt_text = string("HW", num, " Web Instructions")
+    badge_right = "web"
+  elseif ftype == "pdf"
+    link = string("/assignments/hw", num, "/hw", num, ".pdf")
+    alt_text = string("HW", num, " PDF Instructions")
+    badge_right = "pdf"
+  end
+  badge_left = string("HW", num)
+  badge_url = string("https://img.shields.io/static/v1?label=", badge_left, "&message=", badge_right, "&color=b31b1b&labelColor=222222&style=flat")
+  badge_string = string(
+    "[!", "[", alt_text, "]", 
+    "(", badge_url, ")", "]",
+    "(", link, ")"
+  )
+  return badge_string
+end
+
+function rubric_badge(num, ftype)  
+  if ftype == "html"
+    link = string("/assignments/hw", num, "/rubric/")
+    alt_text = string("HW", num, " Web Rubric")
+    badge_right = "web"
+  elseif ftype == "pdf"
+    link = string("/assignments/hw", num, "/rubric.pdf")
+    alt_text = string("HW", num, " PDF Rubric")
+    badge_right = "pdf"
+  end
+  badge_left = "rubric"
+  badge_url = string("https://img.shields.io/static/v1?label=", badge_left, "&message=", badge_right, "&color=b31b1b&labelColor=222222&style=social")
+  badge_string = string(
+    "[!", "[", alt_text, "]", 
+    "(", badge_url, ")", "]",
+    "(", link, ")"
+  )
+  return badge_string
+end
+
+function hfun_hw_badges(params) 
+  num = params[1]
+  io = IOBuffer()
+  write(io, Franklin.fd2html("""
+    @@badges
+    $(hw_badge(0, "html"))
+    $(hw_badge(0, "pdf"))
+    @@
+    """, internal=true)
+  )
+  return String(take!(io))
+end
+
+function hfun_rubric_badges(params) 
+  num = params[1]
+  io = IOBuffer()
+  write(io, Franklin.fd2html("""
+    @@badges
+    $(rubric_badge(0, "html"))
+    $(rubric_badge(0, "pdf"))
+    @@
+    """, internal=true)
+  )
+  return String(take!(io))
+end

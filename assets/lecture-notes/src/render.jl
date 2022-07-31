60218857fbbcea370da8b858d0875a0cb33e554a
vsrikrish <vs498@cornell.edu>
2022-07-31 15:08:31 +0000

    Deploying to gh-pages from  @ 9a6f6240a93b4749f7b17daf07ef44447d62bf01 🚀

--- assets/lecture-notes/src/render.jl
+++ assets/lecture-notes/src/render.jl
@@ -10,5 +10,5 @@ note_paths = setdiff(filter(x -> isdir(x), readdir(".")), paths_ignore)
 for path in note_paths
     path
     Remark.slideshow(path, title = titlecase(replace(path, "-" => " ")), options = Dict("ratio" => "16:9", "highlightStyle" => "github", "highlightLanguage" => "julia"))
-    mv(string(path, "/build/index.html"), string(path, "/index.html"))
+    mv(string(path, "/build/index.html"), string(path, "/index.html"); force=true)
 end
